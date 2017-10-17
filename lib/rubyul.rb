302c140753b59fc6d0f5e3b8a8557b49a35ecce6
William Denton <wtd@pobox.com>
2017-10-17 16:00:23 -0400

    Make it clean for Rubocop

--- lib/rubyul.rb
+++ lib/rubyul.rb
@@ -1,23 +1,23 @@
 require "rubyul/version"
 
-require 'date'
-require 'json'
+require "date"
+require "json"
 
+# All the small helper functions are here.
 module Rubyul
-
   def self.academic_year(date)
+    # Academic years go from September to August.
     (Date.parse(date) << 8).year
   end
 
-  PLATFORM_REGEXES = JSON.parse(File.read(File.dirname(__FILE__) + "/platform_regexes.json"))
+  PLATFORM_REGEXES = JSON.parse(File.read(File.dirname(__FILE__) +
+                                          "/platform_regexes.json"))
 
   def self.hostname_to_platform(hostname)
+    # TODO: Deal with ignores or not, with an option.
     PLATFORM_REGEXES.each do |regexp, platform_name|
-      if Regexp.new(regexp) =~ hostname
-        return platform_name
-      end
+      return platform_name if Regexp.new(regexp) =~ hostname
     end
-    return nil
+    nil
   end
-
 end
